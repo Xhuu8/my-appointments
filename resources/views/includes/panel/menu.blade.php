e623f93b87a094baadec3badee9f5c30f3b46164
jesus sanchez sarmiento <xhuxho.emprende@gmail.com>
2025-07-03 12:32:19 -0600

    iconos, etiquetas y cerrar seccion

---
+++ resources/views/includes/panel/menu.blade.php
@@ -0,0 +1,59 @@
+<!-- Form -->
+<form class="mt-4 mb-3 d-md-none">
+    <div class="input-group input-group-rounded input-group-merge">
+        <input type="search" class="form-control form-control-rounded form-control-prepended" placeholder="Search"
+            aria-label="Search">
+        <div class="input-group-prepend">
+            <div class="input-group-text">
+                <span class="fa fa-search"></span>
+            </div>
+        </div>
+    </div>
+</form>
+<!-- Navigation -->
+<ul class="navbar-nav">
+    <li class="nav-item">
+        <a class="nav-link" href="./index.html">
+            <i class="ni ni-tv-2 text-primary"></i> Dashboard
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="./examples/icons.html">
+            <i class="ni ni-planet text-blue"></i> Especialidades
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="./examples/maps.html">
+            <i class="ni ni-single-02 text-orange"></i> Medicos
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="./examples/profile.html">
+            <i class="ni ni-satisfied text-yellow"></i> Pacientes
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="./examples/tables.html"
+            onclick="event.preventDefault(); document.getElementById('formLogout').submit();">
+            <i class="ni ni-user-run text-red"></i> Logout
+        </a>
+    </li>
+
+</ul>
+<!-- Divider -->
+<hr class="my-3">
+<!-- Heading -->
+<h6 class="navbar-heading text-muted">Documentation</h6>
+<!-- Navigation -->
+<ul class="navbar-nav mb-md-3">
+    <li class="nav-item">
+        <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html">
+            <i class="ni ni-collection text-green"></i> Frecuencia de citas
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html">
+            <i class="ni ni-spaceship text-blue "></i> Medicos más activos
+        </a>
+    </li>
+</ul>
