3b836f5333abc42b2af868b031a150bc65c645ca
jesus sanchez sarmiento <xhuxho.emprende@gmail.com>
2025-07-11 09:23:48 -0600

    tablas de relaciones mochos a muchos

--- app/Models/Specialty.php
+++ app/Models/Specialty.php
@@ -12,4 +12,9 @@ class Specialty extends Model
         'name',
         'description'
     ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function specialties()
+    {
+        return $this->belongsToMany(Specialty::class);
+    }
 }
